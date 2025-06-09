9f83876f4fe0ad6fdba5a2d1ec276148215b136a
Faisal Shaikh <faisals@smartyz.com>
2025-06-09 20:59:50 +0530

    changes in mongodb configuration connection

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 let mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://faisals:1eHnAJrelnOCQnV7@cluster0.jhrpgyy.mongodb.net/Portfolio_Tracker?retryWrites=true&w=majority', {
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true ,
+  useUnifiedTopology: true,
   ssl: true
 })
   .then(() => console.log('MongoDB Connected'))
