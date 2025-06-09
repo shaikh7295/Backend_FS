267f5398caaf4049d286134dc48b94c83d50a633
Faisal Shaikh <faisals@smartyz.com>
2025-06-09 19:27:27 +0530

    added serverless package

--- api/app.js
+++ api/app.js
@@ -4,9 +4,12 @@ const multer = require('multer');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+const serverless = require('serverless-http');
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
-
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel' });
+});
 const path = require('path');
  
 require('../src/config/db.config');
@@ -19,3 +22,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
